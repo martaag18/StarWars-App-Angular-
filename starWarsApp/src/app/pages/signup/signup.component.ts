4b8c23006c33d22ee8e65cff0ba764159ab94794
Marta Altimir <martaag18@gmail.com>
2025-01-21 20:04:32 +0100

    Added reactive register form & Validators

--- starWarsApp/src/app/pages/signup/signup.component.ts
+++ starWarsApp/src/app/pages/signup/signup.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../../shared/logo/logo.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../../shared/interfaces/user.interface';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
-  imports: [LogoComponent],
+  imports: [LogoComponent, ReactiveFormsModule, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
+
 export class SignupComponent {
+
+  registerForm = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    surname: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required]),
+    repeatPassword: new FormControl("", [Validators.required]),
+
+  });
+
+  submitRegister(): void {
+    if(this.registerForm.valid){
+      const user: User = {
+        name: this.registerForm.value.name!,
+        surname: this.registerForm.value.surname!,
+        email: this.registerForm.value.email!,
+        password: this.registerForm.value.password!,
+        repeatPassword: this.registerForm.value.repeatPassword!,
+
+      };
+      console.log("user created", user);
+    }
+  }
 
 }
